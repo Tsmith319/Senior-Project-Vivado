a884109f1547eca468be71572a100badd557d6ef
Taylor Smith <TaySmith16@gmail.com>
2018-11-03 20:44:12 -0600

    Finished initial implementation of mean machine module need to create test bench and interrupt module.

--- mean_machine/mean_machine.srcs/sources_1/new/mean_machine_module.v
+++ mean_machine/mean_machine.srcs/sources_1/new/mean_machine_module.v
@@ -25,12 +25,89 @@ module mean_machine_module(
     input reset,
     input clk,
     input next_section,
+    input buf_selected,
     output latch_select,
     output [10:0] bit_num,
     output gsclk,
     output sclk,
     output latch,
-    output [1:0] buf_select,
+    output reg [1:0] buf_select,
+    output ready,
     input setup
     );
+    
+    reg [10:0] curr_state = 11'd2004;
+    reg [1:0]  setup_complete = 2'b00;
+    reg clock_state = 0;
+    reg active_data = 0;
+    
+    reg [10:0] next_state;
+    reg [1:0] next_setup_complete;
+    reg next_active_data;
+   
+    assign gsclk = enable && (active_data && ~clock_state);
+    assign latch = enable && (~clock_state && (curr_state == 2002));
+    assign sclk  = enable && (~clock_state) && (((curr_state >= 0) && (curr_state <= 1535)) || (curr_state == 2005));
+    assign latch_select = (setup_complete == 2'd2) ? 1'b0 : 1'b1;
+    assign bit_num = curr_state; 
+    assign ready = (curr_state == 11'd2004);
+    
+    always@(posedge clk) begin
+        curr_state = next_state;
+        clock_state = ~clock_state;
+        setup_complete = next_setup_complete;
+        active_data = next_active_data;
+        
+        if(reset) begin
+            curr_state = 11'd2004;
+            clock_state = 0;
+            setup_complete = 2'b00;
+            active_data = 0;
+        end
+    end
+    
+    always@(*) begin
+        next_state = curr_state;
+        next_setup_complete = setup_complete;
+        next_active_data = active_data;
+        
+        case (curr_state)
+            11'd2000: buf_select = 2'd3;
+            11'd2001: buf_select = 2'd3;
+             default: begin
+                        if(setup_complete < 2'd2)
+                            buf_select = 2'd0;
+                        else
+                            buf_select = 1'd1 + buf_selected;
+                      end
+        endcase
+        
+        if(clock_state == 1'b1) begin
+            case (curr_state)
+                11'd2000: next_state = 11'd1535; //first latch select
+                11'd2001: next_state = 11'd0767;  //second latch select
+                11'd2002: begin                  //LAT
+                            if(setup_complete < 2'd2) begin
+                                next_state = 11'd2003;
+                                next_setup_complete = setup_complete + 1'b1;
+                            end
+                            else
+                                next_state = 11'd2004;
+                                
+                            if(setup_complete == 2'd2)
+                                next_active_data = 1'b1;         
+                          end
+                11'd2003: next_state = 11'd2000; //START
+                11'd2004: begin                  //IDLE
+                            if(setup || next_section)
+                                next_state = 11'd2003;
+                          end
+                11'd2005: next_state = 11'd2002; //DONE
+                11'd0000: next_state = 11'd2005; //LSB pix data 0
+                11'd0768: next_state = 11'd2001; //LSB pix data 1
+                 default: next_state = curr_state - 1'b1;  
+            endcase
+        end
+    end
+     
 endmodule
